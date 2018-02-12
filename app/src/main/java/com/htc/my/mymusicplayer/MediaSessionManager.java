d8357b5d67a2b705c98ddea35e228b6f71317726
l1d000 <l1d000@hotmail.com>
2018-02-12 17:22:45 +0800

    Support mediabutton

--- app/src/main/java/com/htc/my/mymusicplayer/MediaSessionManager.java
+++ app/src/main/java/com/htc/my/mymusicplayer/MediaSessionManager.java
@@ -1,13 +1,18 @@
 package com.htc.my.mymusicplayer;
 
 import android.content.Context;
+import android.content.Intent;
 import android.media.MediaMetadata;
 import android.os.Build;
+import android.os.Bundle;
 import android.support.v4.media.MediaMetadataCompat;
 import android.support.v4.media.session.MediaSessionCompat;
 import android.util.Log;
+import android.view.KeyEvent;
 
 import com.htc.my.files.MySong;
+import com.htc.my.files.PlayerConstants;
+import com.htc.my.service.MyMusicPlayerService;
 
 
 public class MediaSessionManager{
@@ -21,6 +26,7 @@ public class MediaSessionManager{
         this.mContext = mContext;
         setupMediaSession();
     }
+
     public static synchronized MediaSessionManager createInstance(Context ctx) {
        	if (instance == null) {
             	instance = new MediaSessionManager(ctx);
@@ -32,7 +38,6 @@ public class MediaSessionManager{
     public static MediaSessionManager getInstance() {
         return instance;
      }
-
     /**
      * API 21 以上 耳机多媒体按钮监听 MediaSessionCompat.Callback
      */
@@ -41,33 +46,58 @@ public class MediaSessionManager{
 //        接收到监听事件，可以有选择的进行重写相关方法
 
         @Override
+        public boolean onMediaButtonEvent(Intent mediaButtonEvent) {
+            String action = mediaButtonEvent.getAction();
+        //    Log.d(TAG, "onMediaButtonEvent");
+            if (action.equals(Intent.ACTION_MEDIA_BUTTON)) {
+                KeyEvent event = (KeyEvent) mediaButtonEvent
+                        .getParcelableExtra(Intent.EXTRA_KEY_EVENT);
+                int keyAction = event.getAction();
+                if (keyAction == KeyEvent.ACTION_DOWN) {
+                    handleKeyEvent(event);
+                } else if (keyAction == KeyEvent.ACTION_UP) {
+                    handleKeyEvent(event);
+                }
+                return true;
+            }
+
+            return super.onMediaButtonEvent(mediaButtonEvent);
+        }
+
+        @Override
         public void onPlay() {
             super.onPlay();
+            Log.d(TAG, "onPlay");
         }
 
         @Override
         public void onPause() {
             super.onPause();
+            Log.d(TAG, "onPause");
         }
 
         @Override
         public void onSkipToNext() {
             super.onSkipToNext();
+            Log.d(TAG, "onSkipToNext");
         }
 
         @Override
         public void onSkipToPrevious() {
             super.onSkipToPrevious();
+            Log.d(TAG, "onSkipToPrevious");
         }
 
         @Override
         public void onStop() {
             super.onStop();
+            Log.d(TAG, "onStop");
         }
 
         @Override
         public void onSeekTo(long pos) {
             super.onSeekTo(pos);
+            Log.d(TAG, "onSeekTo");
         }
     };
 
@@ -114,4 +144,41 @@ public class MediaSessionManager{
         mMediaSession.setMetadata(metaDta.build());
 
     }
+
+
+    private void handleKeyEvent(KeyEvent event) {
+        int keyCode = event.getKeyCode();
+       // Log.d(TAG,"Received ACTION_DOWN with keycode " + keyCode);
+        Bundle msg = new Bundle();
+        if (keyCode == KeyEvent.KEYCODE_MEDIA_PLAY) {
+            Log.d(TAG,"KEYCODE_MEDIA_PLAY ");
+            // 将音乐暂停功能交给后台
+            Intent intent=new Intent(mContext, MyMusicPlayerService.class);
+            intent.putExtra("MSG",PlayerConstants.MSG_CONTINUE);
+            mContext.startService(intent);
+        } else if (keyCode == KeyEvent.KEYCODE_MEDIA_PAUSE) {
+            Log.d(TAG,"KEYCODE_MEDIA_PAUSE ");
+            Intent intent=new Intent(mContext, MyMusicPlayerService.class);
+            intent.putExtra("MSG", PlayerConstants.MSG_PAUSE);
+            mContext.startService(intent);
+
+        } else if (keyCode == KeyEvent.KEYCODE_MEDIA_PLAY_PAUSE) {
+            Log.d(TAG,"KEYCODE_MEDIA_PLAY_PAUSE ");
+        } else if (keyCode == KeyEvent.KEYCODE_MEDIA_STOP) {
+            Log.d(TAG,"KEYCODE_MEDIA_STOP ");
+
+        } else if (keyCode == KeyEvent.KEYCODE_MEDIA_NEXT) {
+            Log.d(TAG,"KEYCODE_MEDIA_NEXT ");
+
+        } else if (keyCode == KeyEvent.KEYCODE_MEDIA_PREVIOUS) {
+            Log.d(TAG,"KEYCODE_MEDIA_PREVIOUS ");
+
+        } else if (keyCode == KeyEvent.KEYCODE_MEDIA_FAST_FORWARD) {
+            Log.d(TAG,"KEYCODE_MEDIA_FAST_FORWARD ");
+        } else if (keyCode == KeyEvent.KEYCODE_MEDIA_REWIND) {
+            Log.d(TAG,"KEYCODE_MEDIA_REWIND ");
+        }
+
+    }
+
 }
